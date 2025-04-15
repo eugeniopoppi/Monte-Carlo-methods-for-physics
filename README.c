63b3a83ad3803c97e76a26df8761310af1cae156
eugeniopoppi <eugeniopoppi@gmail.com>
2025-04-15 18:05:06 +0200

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,5 @@
+//2dU1 is a two dimensional U(1) gauge theory- still working in progress
+//free_scalar_th is a two dimensional quantum scalar field, provide a computation of energy dentisy
+//ising_cluster is an ising model with cluster update
+//xy_auto is a O(2) model with micro and metro updates
+//the header is necessary in all the codes
